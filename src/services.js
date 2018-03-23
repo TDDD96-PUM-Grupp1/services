4e15ad988e119de459140d9ad39ea7ca3520ecf5
Tim Håkansson <34460034+Thraix@users.noreply.github.com>
2018-03-23 10:51:02 +0100

    Communication (#1)
    * Add ability to create different instances.
    
    * Add error message when a UI requests to start an instance with existing name
    
    * Fix eslint
    
    * Add call to request all instances
    
    * Add comments
    
    * Remove unnecessary prints
    
    * Move random name to UI.

--- src/services.js
+++ src/services.js
@@ -1,27 +1,98 @@
 import { createRpcService, typeAssert } from 'ds-node-service';
 
-const serviceName = 'conf-v1';
-const defaultCredentialsUrl = 'http://auth:8009/getAuthToken';
+const fs = require('fs');
 
-function createService(address, runForever, credentialsUrl) {
+const serviceName = 'services';
+const instanceNames = [];
+
+let nouns = [];
+let adjectives = [];
+
+/*
+ * Load in all the dictionary words for the randomizer
+ */
+function initWords() {
+  // Load in nouns
+  fs.readFile('InstanceWordsNouns.txt', (err, data) => {
+    if (err) throw err;
+    nouns = `${data}`.split('\n');
+    for (let i = nouns.length - 1; i >= 0; i -= 1) {
+      // Sometimes there are empty strings in the list. Remove them
+      if (nouns[i] === '') {
+        nouns.splice(i, 1);
+      }
+    }
+  });
+
+  // Load in adjectives
+  fs.readFile('InstanceWordsAdjectives.txt', (err, data) => {
+    if (err) throw err;
+    adjectives = `${data}`.split('\n');
+    for (let i = adjectives.length - 1; i >= 0; i -= 1) {
+      // Sometimes there are empty strings in the list. Remove them
+      if (adjectives[i] === '') {
+        adjectives.splice(i, 1);
+      }
+    }
+  });
+}
+
+/*
+ * @returns true if there is not instance with the given name.
+ */
+function checkInstanceName(name) {
+  for (let i = 0; i < instanceNames.length; i += 1) {
+    if (instanceNames[i].name === name) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+ * Add an instance if it doesn't exist.
+ * @returns false if the given instance name already exists.
+ */
+function addInstanceName(uiId, name) {
+  if (!checkInstanceName(name)) return false;
+
+  instanceNames.push({ id: uiId, name });
+  return true;
+}
+
+/*
+ * Remove an instance when a UI disconnects.
+ * eslint-disable no-unused-vars
+ */function removeInstanceName(uiId) { for (let i = 0; i < instanceNames.length; i += 1) { if (instanceNames[i].id === uiId) instanceNames.splice(i, 1); }
+}
+
+/*
+ * Creates all the rpc calls needed for the services.
+ */
+function createService(address, runForever) {
   const obj = createRpcService({
     serviceName,
     address,
     runForever,
-    credentialsUrl,
   });
 
-  obj.items = dataset(); // Attach whatever needed in any suitable way
-
   obj.registerApi({
-    createItem: {
-      method: ({ id, properties }) => {
+    // Creates an instance with the given name.
+    createInstance: {
+      method: ({ name }) => {
         typeAssert('String', name);
-        typeAssert('{...}', properties);
-        const res = obj.items.create({ id, properties });
-        return res;
+        if (!addInstanceName(obj.client.getUid(), name)) {
+          console.log('Name already exists');
+          return { error: 'Instance already exists' };
+          // Error stuff
+        }
+        return {};
       },
-      argDoc: [['id', 'String'], ['properties', '{...}']],
+    },
+
+    // Returns all the instances as a list of objects.
+    getInstances: {
+      method: () => instanceNames,
     },
   });
 
@@ -29,7 +100,9 @@ function createService(address, runForever, credentialsUrl) {
 }
 
 function main() {
-  service = createService('deepstream:6020', true, defaultCredentialsUrl);
+  initWords();
+  const service = createService('0.0.0.0:60020', true);
   service.start();
 }
+
 if (require.main === module) main();
